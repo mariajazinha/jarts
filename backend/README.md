4b19ee47478271ab2e1267e9c0a55337b60b16de
MariaJazinha <1turmashow@gmail.com>
2021-02-02 20:10:44 -0300

    backend/index.html

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+## API
+criei a pasta jarts no desktop
+abri a pasta no VS Code
+abri o terminal 
+rodei um yarn init -y
+rodei um npm i express nodemon cors axios
+rodei npx lite-server
---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) =>{
+  return res.json([
+    {name: 'Joe'},
+    {name: 'Almeidez'}
+    
+  ])
+})
+
+app.listen('4567')
